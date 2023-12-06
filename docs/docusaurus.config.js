e6221a887f73ede9e7eefc50829623c72af00cb2
gio-rodrigues0 <giovanna.rodrigues@sou.inteli.edu.br>
2023-12-06 17:56:24 -0300

    added the url

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -6,6 +6,9 @@
 
 import {themes as prismThemes} from 'prism-react-renderer';
 
+const organizationName = 'gio-rodrigues0';
+const projectName = 'CodeClub';
+
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'CodeClub',
@@ -13,15 +16,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://${organizationName}.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/${projectName}/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'gio-rodrigues0', // Usually your GitHub org/user name.
+  projectName: 'CodeClub', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
